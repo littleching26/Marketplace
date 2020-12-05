daed7e716692143012550d4a73a229ca1db687ba
Clay <Clay.Lin@ibm.com>
2020-12-05 16:07:12 +0800

    20201205 持續修架構

--- index.py
+++ index.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_home():
     return render_template('index.html')
 
-app.run(debug=True, port='5050')
+
+if __name__ == '__main__':
+    app.run(debug=True)
