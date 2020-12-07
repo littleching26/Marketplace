65eee4626a9920b2e454ba3065df6ff8c15f0dc2
Clay <Clay.Lin@ibm.com>
2020-12-07 23:34:24 +0800

    add route and extract nav-bar

--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -2,17 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
+import { NavBarRoutingModule} from './nav-bar/nav-bar-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NavBarRoutingModule,
     FormsModule
   ],
   providers: [],
--- index.py
+++ index.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
+from flask import jsonify
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_home():
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
