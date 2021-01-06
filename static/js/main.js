32eeb014f28bbd6ff6946084df9398b5e1c4922d
Clay <Clay.Lin@ibm.com>
2021-01-07 01:19:00 +0800

    畫面改版

--- static/js/main.js
+++ static/js/main.js
@@ -4,81 +4,97 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
-(function($) {
-  "use strict";
-  $(document).ready(function () {
-    $('.img-fluid').height($('.img-fluid').width());
-    console.log('--display height--', $('.img-fluid').height());
+
+
+$(document).ready(function () {
+  var resizeTimeout;
+  $('#logoNav').height($(window).height() * 0.07);
+  $('#logoImg').height($(window).height() * 0.1);
+  $('#logoAudio').height($(window).height() * 0.06);
+  $(window).resize(function () {
+    clearTimeout(resizeTimeout)
+    resizeTimeout = setTimeout(function () {
+      $('#logoNav').height($(window).innerHeight() * 0.07);
+      $('#logoImg').height($(window).innerHeight() * 0.1);
+      $('#logoAudio').height($(window).innerHeight() * 0.06);
+    }, 100);
   })
+})
 
-  var burgerMenu = function() {
-    $('.burger').click(function(e) {
-      $(window).scrollTop(0);
-      if (!$('.burger').hasClass('active'))
-        $('.burger').addClass('active');
-      else
-        $('.burger').removeClass('active');
-    });
-  }
-  burgerMenu();
+// (function($) {
+  // "use strict";
+  
 
-  var siteIstotope = function() {
-    var $container = $('#portfolio-grid').isotope({
-      itemSelector: '.item',
-      isFitWidth: true
-    });
+  
 
-    $(window).resize(function() {
-      $container.isotope({
-        columnWidth: '.col-sm-3'
-      });
-    });
+  // var burgerMenu = function() {
+  //   $('.burger').click(function(e) {
+  //     $(window).scrollTop(0);
+  //     if (!$('.burger').hasClass('active'))
+  //       $('.burger').addClass('active');
+  //     else
+  //       $('.burger').removeClass('active');
+  //   });
+  // }
+  // burgerMenu();
 
-    $container.isotope({
-      filter: '*'
-    });
+  // var siteIstotope = function() {
+  //   var $container = $('#portfolio-grid').isotope({
+  //     itemSelector: '.item',
+  //     isFitWidth: true
+  //   });
 
-    $('#filters').on('click', 'a', function(e) {
-      e.preventDefault();
-      var filterValue = $(this).attr('data-filter');
-      $container.isotope({
-        filter: filterValue
-      });
-      $('#filters a').removeClass('active');
-      $(this).addClass('active');
-    });
-  }
-  $(window).on('load', function() {
-    siteIstotope();
-  });
+  //   $(window).resize(function() {
+  //     $container.isotope({
+  //       columnWidth: '.col-sm-3'
+  //     });
+  //   });
 
-  var siteOwlCarousel = function() {
-    $('.testimonial-carousel').owlCarousel({
-      center: true,
-      items: 1,
-      loop: true,
-      margin: 0,
-      autoplay: true,
-      smartSpeed: 1000,
-    });
-  };
-  siteOwlCarousel();
+  //   $container.isotope({
+  //     filter: '*'
+  //   });
 
-  $(window).on('load', function() {
-    AOS.init({
-      easing: 'ease',
-      duration: 1000,
-      once: true
-    });
-  });
+  //   $('#filters').on('click', 'a', function(e) {
+  //     e.preventDefault();
+  //     var filterValue = $(this).attr('data-filter');
+  //     $container.isotope({
+  //       filter: filterValue
+  //     });
+  //     $('#filters a').removeClass('active');
+  //     $(this).addClass('active');
+  //   });
+  // }
+  // $().on('load', function() {
+  //   siteIstotope();
+  // });
 
-})(jQuery);
+  // var siteOwlCarousel = function() {
+  //   $('.testimonial-carousel').owlCarousel({
+  //     center: true,
+  //     items: 1,
+  //     loop: true,
+  //     margin: 0,
+  //     autoplay: true,
+  //     smartSpeed: 1000,
+  //   });
+  // };
+  // siteOwlCarousel();
 
-function searchCarNumber(){
-  window.location.href = 'searchCarNumber'
-}
+  // $(window).on('load', function() {
+  //   AOS.init({
+  //     easing: 'ease',
+  //     duration: 1000,
+  //     once: true
+  //   });
+  // });
 
-function dismissAllModal(){
-  $('#successPaid').modal('hide');
-  $('#surePay').modal('hide');
-}
+// })(jQuery);
+
+// function searchCarNumber(){
+//   window.location.href = 'searchCarNumber'
+// }
+
+// function dismissAllModal(){
+//   $('#successPaid').modal('hide');
+//   $('#surePay').modal('hide');
+// }
