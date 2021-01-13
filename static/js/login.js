7e7d2c56ae091ffef838ba62401db09cfc3ccbd0
Clay <Clay.Lin@ibm.com>
2021-01-13 09:55:23 +0800

    前端修改

--- static/js/login.js
+++ static/js/login.js
@@ -1,31 +1,14 @@
 $(document).ready(function () {    
 });
 function userLoginInfo() {
-    // var firstName = $('#firstName').val();
-    // var secondName = $('#secondName').val();
     var email = $('#inputEmail').val();
     if (validationSubmit()) {
-        // var fullName = firstName.toLowerCase() + ' ' + secondName.toLowerCase();
-        var email = email.toLowerCase();
-        // window.location.href = '/index/' + fullName + '/' + email + '/' + true;
+        email = email.replace(/\s*/g, "");
+        email = email.toLowerCase();
         window.location.href = '/exchange-item/' + email + '/' + true;
     }
     //檢查是否輸入完整
     function validationSubmit() {
-        // if (firstName == '') {
-        //     $('#inputCompleteFirstName').css('visibility', 'visible');
-        //     console.log('test1');
-        //     return false;
-        // } else {
-        //     $('#inputCompleteFirstName').css('visibility', 'hidden');
-        // }
-        // if (secondName == '') {
-        //     $('#inputCompleteSecondName').css('visibility', 'visible');
-        //     console.log('test2');
-        //     return false;
-        // } else {
-        //     $('#inputCompleteSecondName').css('visibility', 'hidden');
-        // }
         if (email == '') {
             $('#inputCompleteEmail').css('visibility', 'visible');
             return false;
--- static/js/main.js
+++ static/js/main.js
@@ -2,27 +2,6 @@ $(document).ready(function () {
   $('.main').css('min-height', $(window).height()-100);
 })
 
-// function addMiniPoint(userName, email, points, pointNum) {
-//   userData = {};
-//   userData['THIS_ROUND_POINTS'] = pointNum;
-//   userData['USER_NAME'] = userName;
-//   userData['EMAIL'] = email;
-//   userData['POINTS'] = parseInt(points);
-//   $.ajax({
-//     type: "POST",
-//     url: "/add-mini-point",
-//     contentType: "application/json",
-//     data: JSON.stringify(userData),
-//     dataType: "json",
-//     success: function (response) {
-//       window.location.href = '/get-click-points/' + pointNum + '/' + response['USER_NAME'] + '/' + email;
-//     },
-//     error: function (err) {
-//       console.log(err);
-//     }
-//   });
-// }
-
 function addMiniPoint(email, points, pointNum) {
   userData = {};
   userData['THIS_ROUND_POINTS'] = pointNum;
